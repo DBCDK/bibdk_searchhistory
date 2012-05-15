6d573027f3afbe137f11b71e5987a1bf474b6b18
martin møller <mmj@dbc.dk>
2012-05-15 15:10:20 +0200

    Adds option for saving and unsaving searches. .install file added that creates necessary table in db

---
+++ bibdk_searchhistory.js
@@ -0,0 +1,51 @@
+/*
+ * @file
+ * JavaScript for bibdk_searchhistory.
+ * Adding Memolist checkbox functionality
+ *
+ */
+
+(function ($) {
+    Drupal.behaviors.bibdk_searchhistory = {        
+        attach: function(context) {
+            var basePath = Drupal.settings.basePath;
+            var currentItem;
+            var checked
+            
+            $('.memolist-check').change(function(){ 
+                currentItem = $(this);
+                currentItem.attr('disabled', true);
+                
+                var string = $(this).attr('string');
+                var count = $(this).attr('count');
+                var timestamp = $(this).attr('timestamp');
+                
+                checked = 0;
+                if($(this).attr('checked')){
+                    checked = 1;
+                }
+                
+                var url = "bibdk_searchhistory/memoitemchanged/" + string + "/" + count + "/" + checked + "/" + timestamp;
+                jQuery.ajax({
+                    type: 'GET',
+                    url:basePath + url,
+                    success: onResponse
+                });
+            });
+            
+            function onResponse(data){
+                if(data){
+                    alert(data);
+                    if(checked){
+                        currentItem.attr('checked', false);
+                    } else {
+                        currentItem.attr('checked', true);
+                    }
+                }
+                currentItem.removeAttr('disabled');
+                currentItem = null;
+                checked = null;
+            }
+        }
+    };
+})(jQuery);
