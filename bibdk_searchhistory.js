309c470e84fee514ab9fc7c2fe068c0945edf706
Martin Møller <mmj@dbc.dk>
2012-11-20 15:07:22 +0100

    Hiding combine-functionality due to US #549

--- bibdk_searchhistory.js
+++ bibdk_searchhistory.js
@@ -6,46 +6,46 @@
  */
 
 (function ($) {
-    Drupal.behaviors.bibdk_searchhistory = {        
-        attach: function(context) {
+    Drupal.behaviors.bibdk_searchhistory = {
+        attach:function (context) {
             var basePath = Drupal.settings.basePath;
             var currentItem;
             var shouldSave;
-            
+
             //memolist stuff
-            $('.memo-list-btn').click(function(){
-                
+            $('.memo-list-btn').click(function () {
+
                 currentItem = $(this);
                 $('.memo-list-btn').attr('disabled', 1);
                 var string = currentItem.attr('data-string');
                 var count = currentItem.attr('data-count');
                 var timestamp = currentItem.attr('data-timestamp');
                 var saved = currentItem.attr('data-saved');
-                
+
                 shouldSave = 1;
-                
-                if(saved){
+
+                if (saved) {
                     shouldSave = 0;
                 }
-                
+
                 var url = "bibdk_searchhistory/memoitemchanged/" + string + "/" + shouldSave + "/" + count + "/" + timestamp;
                 url = escape(url);
-                
+
                 jQuery.ajax({
-                    type: 'GET',
+                    type:'GET',
                     url:basePath + url,
-                    success: onResponse
+                    success:onResponse
                 });
                 return false;
             });
-            
-            function onResponse(data){
-                if(data){
+
+            function onResponse(data) {
+                if (data) {
                     alert(data);
-                        
+
                 }
 
-                if(shouldSave){
+                if (shouldSave) {
                     currentItem.attr('data-saved', 1);
                     currentItem.attr('value', Drupal.t('Remove'));
                 } else {
@@ -53,41 +53,41 @@
                     currentItem.attr('value', Drupal.t('Add'));
                 }
                 $('.memo-list-btn').removeAttr('disabled');
-                
-                
+
+
                 currentItem = null;
                 selected = null;
             }
-            
+
             //Select/combine stuff
             //
             //header row
-            $('.select-all').change(function(){
-                window.setTimeout(countSelected, 100);
+            $('.select-all').change(function () {
+                //window.setTimeout(countSelected, 100);
             });
-            
+
             //single rows
-            $('td div .combine-select').change(function(){
-                countSelected()
+            $('td div .combine-select').change(function () {
+                //countSelected()
             });
-            
-            function countSelected(){
-                var b = $('td div .combine-select');                
-                
+
+            function countSelected() {
+                var b = $('td div .combine-select');
+
                 var count = b.filter(':checked').length;
-                
-                if(count >= 1){
+
+                if (count >= 1) {
                     $('#edit-delete').removeAttr('disabled');
                     $('#edit-combine').attr('disabled', 1);
-                    if(count >= 2){
-                        $('#edit-combine').removeAttr('disabled');    
+                    if (count >= 2) {
+                        $('#edit-combine').removeAttr('disabled');
                     }
                 } else {
                     $('#edit-delete').attr('disabled', 1);
                     $('#edit-combine').attr('disabled', 1);
                 }
             }
-            
+
         }
     };
 })(jQuery);
